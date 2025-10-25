e09f64a23f8fed6288dbf7c7b28cd26de202b1c3
Maria-Sultana-Meem <sultanamari639@gmail.com>
2025-10-25 12:32:51 +0600

    fully complete

--- src/components/ExtraSectionReviews.jsx
+++ src/components/ExtraSectionReviews.jsx
@@ -35,10 +35,11 @@ const ExtraSectionReviews = () => {
         <p className="text-gray-600 mt-2 text-center text-2xl font-semibold">See what our lovely customers say about WarmPaws!</p>
             <div data-aos="zoom-out" className='grid grid-cols-1 mt-5 md:grid-cols-3 gap-5'>
                 {
-                    reviews.map(item=><div key={item.id} className='p-6 bg-blue-100 rounded-md text-center space-y-3'>
+                    reviews.map(item=><div key={item.id} className='p-6 bg-red-100/50 rounded-md text-center space-y-3'>
                       <div className='flex justify-center'><img src={item.image} alt="" className='rounded-full w-40 h-40'/></div>
                        <h3 className='font-bold text-2xl'>{item.name}</h3>
-                       <p className='font-semibold text-2xl flex justify-center'><span className='text-red-500'>{item.pet} <span className='flex justify-center items-center'>{item.rating}<FaStar className='text-yellow-500'></FaStar></span></span></p>
+                       <p className='font-semibold text-2xl flex justify-center'><span className='text-blue-700'>{item.pet}
+                         <span className='flex justify-center items-center text-red-500'>Rating:{item.rating}<FaStar className='text-yellow-500'></FaStar></span></span></p>
                        <p className='text-gray-500'>{item.comment}</p>
                     </div>)
                 }
